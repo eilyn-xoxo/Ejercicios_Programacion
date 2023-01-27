dfc61a20388f81fab118b6a29a2d33007126781d
eilyn-xoxo <97348139+eilyn-xoxo@users.noreply.github.com>
2023-01-26 23:27:41 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _6
+namespace _2._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
             int[] numeros = new int[10];
+            int valorMax = numeros[0];
             int posicion = 0;
 
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i > 10; i++)
             {
-                if (numeros[i] >= 1000)
+                if (numeros[i] % 2 == 1)
                 {
+                    if (numeros[i] > valorMax)
+                        valorMax = numeros[i];
                     posicion = i;
-                    Console.WriteLine($"En esta posicion hay un valor mayor de tres digitos en la posicion {posicion}");
                 }
+            }
+            Console.WriteLine($"El numero primo mayor se encuentra en la  posicion {posicion} ");
+        }
+    }
+}
+
+        }
     }
 }
