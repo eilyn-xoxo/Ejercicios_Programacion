c9407097f98cccc76628aceb12659fedddb8ad05
eilyn-xoxo <97348139+eilyn-xoxo@users.noreply.github.com>
2023-01-26 23:27:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[10];
+            int posicion = 0;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[i] >= 1000)
+                {
+                    posicion = i;
+                    Console.WriteLine($"En esta posicion hay un valor mayor de tres digitos en la posicion {posicion}");
+                }
+    }
+}
