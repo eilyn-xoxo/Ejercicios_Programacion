b4405518fa52dd98c53e7105a29ead57d005aaca
eilyn-xoxo <97348139+eilyn-xoxo@users.noreply.github.com>
2023-01-26 23:31:19 -0400

    Add files via upload

---
+++ E8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[10] ;
+
+            int negativos = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (numeros[i] < 0)
+                {
+
+                    negativos = negativos + 1;
+                }
+            }
+            Console.WriteLine($"En el arreglo {negativos} numeros negativos");
+
+        }
+    }
+}
+        }
+    }
+}
