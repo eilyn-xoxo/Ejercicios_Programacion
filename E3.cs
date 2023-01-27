68195eeef4d971ceaec2fedc79c527ef4fbc48b0
eilyn-xoxo <97348139+eilyn-xoxo@users.noreply.github.com>
2023-01-26 23:02:08 -0400

    Add files via upload

---
+++ E3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arreglo = new int[10] { 34, 54, 8, 0, 56, 76, 8, 9, 2, 3 };
+            int valorMax = arreglo[0];
+            int posicion = -1;
+            int primo = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (primo(arreglo[i]))
+                {
+                    if (arreglo[i] > valorMax || !primo(valorMax))
+                    {
+                        valorMax = arreglo[i];
+                        posicion = i;
+                    }
+                }
+            }
+            if (posicion == -1)
+            {
+                Console.WriteLine("No existen numeros primos en el listado");
+            }
+            else
+            {
+                Console.WriteLine($"El numero primo mayor se encuentra en la posicion {posicion}");
+            }
+        }
+    }
+    }
+}
